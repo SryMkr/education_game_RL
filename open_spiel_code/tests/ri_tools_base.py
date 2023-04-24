3547a0ace03d368bbbe09ad4be53b65f2973a4f5
SryMkr <73199151+SryMkr@users.noreply.github.com>
2023-04-24 14:29:30 +0100

    Create ri_tools_base.py
    改变游戏里面变量的算法

---
+++ open_spiel_code/tests/ri_tools_base.py
@@ -0,0 +1,77 @@
+"""Reinforcement Learning (RL) tools Open Spiel."""
+
+import abc  # 抽象类，只能继承
+
+
+class ValueSchedule(metaclass=abc.ABCMeta):
+    """Abstract base class changing (decaying) values."""
+    #  什么是decaying value
+    @abc.abstractmethod
+    def __init__(self):
+        """Initialize the value schedule."""
+
+    @abc.abstractmethod
+    def step(self):
+        """Apply a potential change in the value.
+    This method should be called every time the agent takes a training step.
+    Returns:
+      the value after the step.
+    """
+
+    @property
+    @abc.abstractmethod
+    def value(self):
+        """Return the current value."""
+
+
+# 这个是常量，整个游戏都不改变的值
+class ConstantSchedule(ValueSchedule):
+    """A schedule that keeps the value constant."""
+
+    def __init__(self, value):
+        super(ConstantSchedule, self).__init__()
+        self._value = value
+
+    def step(self):
+        return self._value
+
+    @property
+    def value(self):
+        return self._value
+
+
+# 所以这个线性时间表是用来生成变量的
+class LinearSchedule(ValueSchedule):
+    """A simple linear schedule."""
+
+    def __init__(self, init_val, final_val, num_steps):
+        """A simple linear schedule.
+    Once the the number of steps is reached, value is always equal to the final
+    value.
+    Arguments:
+      init_val: the initial value.
+      final_val: the final_value
+      num_steps: the number of steps to get from the initial to final value.
+    """
+        super(LinearSchedule, self).__init__()
+        self._value = init_val  # 一个数的初始值
+        self._final_value = final_val   # 一个数的终止值
+        assert isinstance(num_steps, int)  # 判断这个值是不是int类型
+        self._num_steps = num_steps  # 获得一个时间步长
+        self._steps_taken = 0  # 记录已经走了多少步
+        self._increment = (final_val - init_val) / num_steps  # 每一步的增加值是多少
+
+    # 走一步需要更新的变量
+    def step(self):
+        self._steps_taken += 1  # 记录走了多少步
+        if self._steps_taken < self._num_steps:   # 如果还没有到结尾
+            self._value += self._increment  # 每走一步加一个增量
+        elif self._steps_taken == self._num_steps:  # 如果已经走到了结尾
+            self._value = self._final_value  # 那直接将重点值给最后一步就成
+        # 返回当前步的值
+        return self._value
+
+    #  返回当前步的值
+    @property
+    def value(self):
+        return self._value
