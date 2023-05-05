e83c28a9d59aa706677b8af76123c98c97ec15b2
SryMkr <73199151+SryMkr@users.noreply.github.com>
2023-05-05 17:43:53 +0100

    Update Word_Maker.py

--- open_spiel_code/My_Module/Word_Maker.py
+++ open_spiel_code/My_Module/Word_Maker.py
@@ -1,8 +1,9 @@
 """
+please ignore all the chinese characters that is for informing me something.
 # 想一下如何利用学生的准确性和完整度
 # 学生不知道正确答案是什么
 # 我的payoff是什么？才能达到我的目的
-problem 1： student do not forget anything, if student get correct, all get correct in all levels.
+problem 1： student do not forget anything. currently, if student get correct, all get correct in all levels.
 """
 
 import random
@@ -13,9 +14,10 @@ import pyspiel
 from random import choice
 import copy
 
-_TASKS_POOL: List[str] = ['bow', 'sink', 'crane', 'tenant', 'condemn', 'expedite']  # the tasks pool
+# the tasks pool, tutor agent randomly select task from here
+_TASKS_POOL: List[str] = ['bow', 'sink', 'crane', 'tenant', 'condemn', 'expedite']
 
-# [letter,[confusing letters]]
+# every letter have confusing letter list that used to confuse student.
 _CONFUSING_LETTER_DIC: Dict[str, List[str]] = {'a': ['e', 'i', 'o', 'u', 'y'], 'b': ['d', 'p', 'q', 't'],
                                                'c': ['k', 's', 't', 'z'], 'd': ['b', 'p', 'q', 't'],
                                                'e': ['a', 'o', 'i', 'u', 'y'], 'f': ['v', 'w'], 'g': ['h', 'j'],
@@ -27,44 +29,48 @@ _CONFUSING_LETTER_DIC: Dict[str, List[str]] = {'a': ['e', 'i', 'o', 'u', 'y'], '
                                                'x': ['s', 'z'], 'y': ['e', 'i'], 'z': ['c', 's']}
 
 
+# chance agent randomly select a task word from tasks pool
 class ChanceAction:
-    def __init__(self, tasks_pool: List[str]) -> None:  # input: tasks pool list
-        self._tasks_pool: List[str] = tasks_pool  # get the task list
+    def __init__(self, tasks_pool: List[str]) -> None:  # tasks pool list
+        self._tasks_pool: List[str] = tasks_pool  # initial the task list
         self._task_word: str = random.choice(self._tasks_pool)  # randomly select a task
 
     @property
-    def get_task_word(self) -> str:
+    def get_task_word(self) -> str:  # the action of chance agent will select a task word
         return self._task_word
 
 
+# tutor action
 class TutorAction:
     def __init__(self):
-        self.letters_pool: List[str] = []  # the letters students get
+        self.letters_pool: List[str] = []  # tutor deal letters for student agent
         self.confusing_letter_pool: List[str] = []  # store confusing letter
 
     # input: confusing letters,  output: available letter for student agent
     def deal_letters(self, task_word: str, confuse_letters: int = 0) -> Tuple[List[str], int]:
         if confuse_letters:  # with confusing letter
-            self.letters_pool: List[str] = [correct_letter for correct_letter in task_word]  # correct letters
+            self.letters_pool: List[str] = [correct_letter for correct_letter in task_word]  # get correct letters
             for letter in self.letters_pool:
-                self.confusing_letter_pool.append(random.choice(_CONFUSING_LETTER_DIC[letter]))
+                self.confusing_letter_pool.append(random.choice(_CONFUSING_LETTER_DIC[letter])) # select confusing letter
                 # correct letter + confusing letter
             self.letters_pool: List[str] = self.letters_pool + self.confusing_letter_pool
         else:  # without confusing letter
-            self.letters_pool: List[str] = [letter for letter in task_word]  # letters
-        random.shuffle(self.letters_pool)
+            self.letters_pool: List[str] = [letter for letter in task_word]  # only get the correct letter
+        random.shuffle(self.letters_pool)  # shuffle the order of letter
+        #  return the available letter for student agent, the length of word
         return self.letters_pool, len(task_word)
 
-    # tutor agent decide
+    # tutor agent decide which difficulty level to deliver
     def adjust_difficulty_level(self, difficulty: int) -> Tuple[int, int]:
         difficult_level: Dict[int, Dict[str, int]] = {1: {'attempts': 4, 'confusing_letter': 0},
                                                       2: {'attempts': 3, 'confusing_letter': 1},
                                                       3: {'attempts': 2, 'confusing_letter': 1},
                                                       4: {'attempts': 1, 'confusing_letter': 1}}
+        # return the [total number of attempts students get in each round, confusing letter setting]
         return difficult_level[difficulty]['attempts'], difficult_level[difficulty]['confusing_letter']
 
 
-# input: student spelling, answer  output:feedback
+# examiner player
 class ExaminerAction:
     def __init__(self):
         self.red: Dict[str, str] = {}  # record wrong letter {correct_letter:[position,confusing_letter]}
@@ -76,16 +82,16 @@ class ExaminerAction:
     # input： student agent spelling, output: completeness, accuracy, feedback information
     def check_spelling(self, student_agent_spelling: str, answer: str) -> Tuple[
         Dict[str, str], Dict[str, str], Dict[str, str], float, float]:
-        for index in range(len(answer)):  # 尽量记录字母, 同一个字母会有重写的问题
-            if student_agent_spelling[index] not in answer:  # not in correct letter
+        for index in range(len(answer)):  #
+            if student_agent_spelling[index] not in answer:  # the letter not in correct letter labeled red
                 self.red[answer[index] + '_' + str(index)] = student_agent_spelling[index]
-            elif student_agent_spelling[index] == answer[index]:  # correct at the correct position
+            elif student_agent_spelling[index] == answer[index]:  # correct at the correct position labeled green
                 self.green[answer[index] + '_' + str(index)] = student_agent_spelling[index]
-            else:  # correct letter at the wrong place
+            else:  # correct letter at the wrong place labeled yellow
                 self.yellow[student_agent_spelling[index] + '_' + str(index)] = answer[index]
-        self.accuracy = Levenshtein.ratio(student_agent_spelling, answer)
+        self.accuracy = Levenshtein.ratio(student_agent_spelling, answer)  # evaluate accuracy
         self.completeness = 1 - Levenshtein.distance(student_agent_spelling, answer) / len(
-            answer)
+            answer)  # evaluate completeness
         return self.red, self.green, self.yellow, self.accuracy, self.completeness
 
 
@@ -96,14 +102,14 @@ class StudentAction:
         self.word_length: int = word_length  # word length
         self.memory: Dict[str:str] = {}  # student agent memory
         self.available_index: List[int] = []  # corresponding to the available letter
-        self.available_letters_copy: List[int] = []
+        self.available_letters_copy: List[int] = []  # copy the available letter
 
-    # learn from previous mistake, (accuracy+completeness)/(2-accuracy-completeness)>0.8
+    # learn from previous mistake
     def learn_from_history(self, red_letters: Dict[str, str], green_letters: Dict[str, str],
                            yellow_letters: Dict[str, str], spelling_accuracy: float,
                            spelling_completeness: float) -> str:
         self.available_index = [i for i in range(self.word_length)]
-        # for red letter, remove red letters
+        # for red letter, permanent remove red letters
         for _, values in red_letters.items():  # red letter
             self._available_letters.remove(values[0])  # red letter remove from available letters
         self.available_letters_copy: List[str] = copy.deepcopy(
@@ -111,18 +117,18 @@ class StudentAction:
 
         # for green letters, take up index and letter
         for key, values in green_letters.items():
-            green_letter, green_letter_index = key.split('_')
+            green_letter, green_letter_index = key.split('_')  #
             self.memory[int(green_letter_index)] = green_letter  # [index:letter]
             self.available_index.remove(int(green_letter_index))
             self.available_letters_copy.remove(green_letter)
 
-        # 黄色的字母说明字母对了，那么选词空间除了绿色以外的位置，但是不能再原来的位置，其他位置都可选择,但是不能从原来的位置删除
+        # yellow letter，
         if yellow_letters:
             yellow_letter_index: List[str] = list(yellow_letters.keys())
-            for item in yellow_letter_index:  # 获得所有的黄色标签
-                yellow_available_index: List[int] = copy.deepcopy(self.available_index)  # 所有黄色标签可以使用的位置
-                letter_index: int = int(item.split('_')[1])  # 获得当前的索引
-                # 后面那个条件是因为可能恰好出现，只能有一个位置使用的情况，而这个位置的索引和之前的位置一样，没有办法
+            for item in yellow_letter_index:  # get the index letter
+                yellow_available_index: List[int] = copy.deepcopy(self.available_index)  # copy the available index letter
+                letter_index: int = int(item.split('_')[1])  # get the previous letter
+                # yellow index cannot take up the index
                 if letter_index in self.available_index and letter_index != self.available_index[0]:
                     yellow_available_index.remove(letter_index)  # 得把当前的索引移除了，从剩下的索引中选择
                 _current_index: int = random.choice(yellow_available_index)
@@ -139,9 +145,10 @@ class StudentAction:
         self.student_spelling = ''
         for index, letter in list_tuple_spelling:
             self.student_spelling += letter  # student new spelling
+        # return student spelling
         return self.student_spelling
 
-    def spell_word(self) -> str:  # spell word，在以及记住的基础上，再填补单词
+    def spell_word(self) -> str:  # spell word, randomly spelling
         # first spelling
         letter_indexes: List[int] = [index for index in range(len(self._available_letters))]  # get all index
         while len(self.student_spelling) < self.word_length:
@@ -149,16 +156,19 @@ class StudentAction:
             chosen_letter: str = self._available_letters[chosen_index]
             self.student_spelling += chosen_letter
             letter_indexes.pop(letter_indexes.index(chosen_index))
+        # return student spelling
         return self.student_spelling
 
-
+# terminal player
 class TerminalAction:
     STOP: bool = True
 
 
+# index number to player
 PLAYERS: Dict[int, any] = {-1: 'chance_player', -4: 'terminal_player', 1: 'tutor_player',
                            2: 'student_player', 3: 'examiner_player'}
 
+# index number to agent player
 _ACTION_SPACE: Dict[int, List[str]] = {-1: ['get_task_word'], 1: ['deal_letters', 'adjust_difficulty_level'],
                                        2: ['spell_word', 'learn_from_history'],
                                        3: ['check_spelling'], -4: ["STOP"]}
@@ -218,13 +228,13 @@ class WordMakerState:
         self._current_task: str = ''  # the current task
         self.task_word_length: int = 0  # the word length
         self._game_over: bool = False  # decide game is over or not
-        self.student_available_letter: List[str] = []
+        self.student_available_letter: List[str] = []  # available letter student get
         self._student_agent_spelling: str = ''  # get student spelling
         self.history: Dict = {}  # record the history state information
         self._next_player: int = -1  # the initial first player is chance player
         self.current_difficulty_level: int = 1  # initial difficulty level
-        self.total_attempts: int = 0
-        self.confusing_letter_setting: int = 0
+        self.total_attempts: int = 0  #  the total numbers of attempts students get
+        self.confusing_letter_setting: int = 0  # confusing letter setting
         self.student_answer_feedback: Tuple[any] = ()
         self.current_game_round: int = 1  # initialize the game round
         self.student_answer_accuracy: float = 0.  # record student spelling
@@ -232,8 +242,7 @@ class WordMakerState:
         self.difficulty_level_decided: bool = False  # decide difficulty
         self.next_attempt: int = 0  # record the next students attempt
 
-    # how to print the legal action
-
+    # get the legal action according to the current player
     def legal_actions(self, player: int) -> str:  # get the legal action
         if player == -1:  # chance player
             self.player_legal_action = 'get_task_word'
@@ -251,18 +260,21 @@ class WordMakerState:
             self.player_legal_action = 'check_spelling'
         return self.player_legal_action
 
+    #  return the current player
     def current_player(self) -> int:
         """Returns id of the next player to move, or TERMINAL if game is over."""
         if self._game_over:
-            # terminal player
+            # -4 represents terminal player
             return -4
         elif not self._current_task:  # if current task is empty, return chance player
+            # -1 represents chance player
             return -1
         else:  # otherwise, other player
             return self._next_player
 
     # 要加切换玩家的动作
-    def apply_action(self, action):  # input:action,而且必须得切换玩家
+    def apply_action(self, action):  # input:action
+        # get the current player
         current_player_id: List[int] = [index for index, values in _ACTION_SPACE.items() if action in values]
         if current_player_id[0] == -1:  # if chance player
             if self.current_game_round == 1:  # select task word only in first game round
@@ -272,7 +284,7 @@ class WordMakerState:
 
         elif current_player_id[0] == -4:  # if terminal player，only used for terminate
             self.terminal_player = TerminalAction()  # instance
-            self._game_over: str = getattr(self.terminal_player, action)
+            self._game_over: str = getattr(self.terminal_player, action)  # game over
             self._next_player = -1  # after terminating, chance player need to choose a task word
 
         elif current_player_id[0] == 1:  # if tutor player
@@ -280,25 +292,26 @@ class WordMakerState:
             if action == 'deal_letters':
                 self.student_available_letter, self.task_word_length = getattr(self.tutor_player, action) \
                     (self._current_task, self.confusing_letter_setting)
-                self._next_player = 2  # 发了单词以后，就要到学生拼写单词了
+                self._next_player = 2  # after deal letter, student agent spell word
             else:
                 self.total_attempts, self.confusing_letter_setting = getattr(self.tutor_player, action) \
                     (self.current_difficulty_level)
-                self._next_player = 1  # 决定了难度之后，还要分发字母
-                self.difficulty_level_decided = True  # 难度已经确定
+                self._next_player = 1  # decide difficulty level, then tutor agent deal letters
+                self.difficulty_level_decided = True  # difficulty settled
 
         elif current_player_id[0] == 2:  # if student player
             self.student_player = StudentAction(self.student_available_letter, self.task_word_length)  # instance
-            if action == 'spell_word':
+            if action == 'spell_word':  # randomly select letter
                 self._student_agent_spelling = getattr(self.student_player, action)()
             else:
+                # learn from history
                 self._student_agent_spelling = getattr(self.student_player, action)(*self.student_answer_feedback)
             self.next_attempt += 1  # student attempt plus one
-            self._next_player = 3  # 学生给了答案以后轮到打分的老师
-            # print(self._student_agent_spelling)
+            self._next_player = 3  # after spelling, next players will be the examiner
 
         else:  # if examiner player
             self.examiner_player = ExaminerAction()  # instance
+            # get the feedback (red, yellow, green, accuracy, completeness)
             self.student_answer_feedback = getattr(self.examiner_player, action)(self._student_agent_spelling,
                                                                                  self._current_task)
             _, _, _, self.student_answer_accuracy, _ = self.student_answer_feedback
@@ -306,24 +319,24 @@ class WordMakerState:
                 if self.next_attempt < self.total_attempts:  # run out of total attempts?
                     self._next_player = 2  # student continue to spelling
 
-                else:  # 如果机会次数已经用完了
-                    if self.current_game_round < 4:  # 要判断游戏结束没有
+                else:  # run out of total attempts
+                    if self.current_game_round < 4:  # game over or not?
                         self.current_game_round += 1  # game round plus one
                         self.current_difficulty_level += 1  # difficulty plus one
-                        self.difficulty_level_decided = False  # 还没决定难度
+                        self.difficulty_level_decided = False  #
                         self._next_player = 1  # tutor agent
-                        self.next_attempt = 0  # 机会次数也要归零重新计数
-                    else:  # 如果已经到了第四轮
+                        self.next_attempt = 0  # initial attempt
+                    else:  # the game round end
                         self.current_game_round = 1
                         self._next_player = -4
 
-            else:  # 如果学生答对了题目
-                if self.current_game_round < 4:  # 如果没有到第四轮
+            else:  # correct spelling
+                if self.current_game_round < 4:  # do not reach round 4
                     self.current_difficulty_level += 1  # difficulty plus one
-                    self.difficulty_level_decided = False  # 还没决定难度
-                    self.current_game_round += 1  # 游戏论次也要加一
-                    self._next_player = 1  # 需要老师重新发单词，决定难度
-                    self.next_attempt = 0  # 机会次数也要归零重新计数
+                    self.difficulty_level_decided = False
+                    self.current_game_round += 1  # game round plus one
+                    self._next_player = 1  # the next player tutor agent to decide difficulty
+                    self.next_attempt = 0  # initial attempt
 
                 else:  # 如果是最后一轮答对的
                     self.current_game_round = 1
@@ -332,11 +345,11 @@ class WordMakerState:
     def is_terminal(self):  # want to know the game terminate or not
         return self._game_over
 
-    def rewards(self):  # 如何奖励老师给了正确的难度，不仅能最快的到达四难度，而且还能还能保持每一关的正确率
+    def rewards(self):  # the rewards function is
         """Total reward for each player over the course of the game so far."""
         pass
 
-    # 打印一些信息
+    # print information
     def __str__(self):
         return f"player_action:{self.player_legal_action},next_player:{PLAYERS[self._next_player]}, current_task:{self._current_task},task_word_length:{self.task_word_length}," \
                f"game_state:{self._game_over},current_difficulty_level:{self.current_difficulty_level}, current_round:{self.current_game_round}," \
@@ -345,7 +358,7 @@ class WordMakerState:
                f"next_attempts:{self.next_attempt},student_answer_feedback:{self.student_answer_feedback}," \
                f" "
 
-
+# iterate information
 for i in range(10):
     print(i)
     state = WordMakerState(tasks_pool=_TASKS_POOL)
@@ -353,6 +366,8 @@ for i in range(10):
         state.apply_action(state.legal_actions(state._next_player))
         print(state)
 
+
+# ------------------------the below are observation information, you can ignore below information--------------------
 _LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
 
