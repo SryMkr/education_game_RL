0c25645e9947ccdfecca456a16f72cb7e2a1b0a6
SryMkr <73199151+SryMkr@users.noreply.github.com>
2023-06-15 09:07:22 +0100

    Add files via upload

--- open_spiel_code/Word_Maker_Interface/agents_interface.py
+++ open_spiel_code/Word_Maker_Interface/agents_interface.py
@@ -1,37 +1,29 @@
 """
-define every agent
+1: agent base abstract class, and per agent interface
+2: initial some parameters that will be regularly used in agents instance
 """
 
 import abc
-from typing import List, Tuple, Dict
-
-# 对于我自己的情况，我得创建一个字典，每一个时间步长得agent是谁，agent得动作
+from typing import List, Tuple, Dict, Union
 
 
-class ChanceInterface(metaclass=abc.ABCMeta):
-    """
-    formulate the chance player interface
-    step 1: Chance Player get the pairs of input and output [chinese, phonetic] -> [spelling]
-    step 2: Chance Player randomly select a pair of [chinese, phonetic] -> [spelling]
-    step 3: if the game terminates, it is either terminate or to step 2
-    :return: the one pair of [chinese, phonetic] -> [spelling]
-    """
+class AgentAbstractBaseClass(metaclass=abc.ABCMeta):
+    """Abstract base class for all agents."""
 
     @abc.abstractmethod
-    def __init__(self, tasks_pool: Dict[str, str], player_id: int, player_name: str = 'student agent'):
+    def __init__(self,
+                 player_id: int,
+                 player_name: str,
+                 **agent_specific_kwargs):
         """Initializes agent.
 
-        Args:
-            tasks_pool: Dictionary, mandatory {'人的 h j u m ʌ n': 'h u m a n', ...............}.
-            player_id: integer, mandatory.
-            player_name: string. Defaults to `student agent`.
-
-        """
-        self.tasks_pool: Dict[str, str] = tasks_pool  # {'人的 h j u m ʌ n': 'h u m a n', ...............}
-        self.ch_pho: str = ''  # store the chinese and phonetic '人的 h j u m ʌ n'
-        self.word: str = ''  # store the english spelling 'h u m a n'
-        self._player_id: int = player_id  # agent ID
-        self._player_name: str = player_name  # agent name
+                Args:
+                    player_id: integer, mandatory.
+                    player_name: string. Defaults to `student agent`.
+                    **agent_specific_kwargs: optional extra args.
+                """
+        self._player_id: int = player_id
+        self._player_name: str = player_name
 
     @property
     def player_id(self) -> int:
@@ -41,97 +33,156 @@ class ChanceInterface(metaclass=abc.ABCMeta):
         return self._player_id
 
     @property
-    def legal_tasks(self) -> Dict[str, str]:
+    def player_name(self) -> str:
         """
-               :return: the legal tasks pool
-               """
-        return self.tasks_pool
+        :return: the player_name
+        """
+        return self._player_name
 
-    @property
+
+class ChanceInterface(AgentAbstractBaseClass):
+    """
+    formulate the chance player interface
+    step 1: Chance Player get the pairs of input and output [chinese, phonetic] -> [spelling]
+    step 2: Chance Player randomly select a pair of [chinese, phonetic] -> [spelling]
+    step 3: if the game terminates, it is either terminate or to step 2
+    :return: the one pair of [chinese, phonetic] -> [spelling]
+    """
+
+    @abc.abstractmethod
+    def __init__(self,
+                 player_id: int,
+                 player_name: str,
+                 tasks_pool: Dict[str, str]):
+        super().__init__(player_id, player_name)
+        """Initializes chance agent.
+
+        Args:
+            tasks_pool: Dictionary, mandatory {'人的 h j u m ʌ n': 'h u m a n', ...............}.
+            self._ch_pho: string, '人的 h j u m ʌ n'
+            self._word: string. 'h u m a n'
+
+        """
+        self._tasks_pool: Dict[str, str] = tasks_pool
+        self._ch_pho: str = ''
+        self._word: str = ''
+
     @abc.abstractmethod
     def select_word(self) -> Tuple[str, str]:
         """
-        select the task
             :return: the task pair
             """
-        return self.ch_pho, self.word
+        return self._ch_pho, self._word
 
 
-class TutorInterface(metaclass=abc.ABCMeta):
+class TutorInterface(AgentAbstractBaseClass):
     @abc.abstractmethod
-    def __init__(self):
+    def __init__(self,
+                 player_id: int,
+                 player_name: str):
+        super().__init__(player_id, player_name)
+        """Initializes tutor agent.
+
+        Args:
+            self.difficulty_levels_definition: Dictionary, mandatory, the difficulty definition
+            self.current_difficulty_level: integer, the initial difficulty level
+            self.legal_difficulty_level: store available letter 
+
         """
-        define difficulty level
-        """
-        self.difficulty_levels_definition: Dict[int, Dict[str, int]] = {}  # define the difficulty level
-        self.current_difficulty_level: int = 1  # initial difficulty level
+        self.difficulty_levels_definition: Dict[int, Dict[str, int]] = {}
+        self.current_difficulty_level: int = 1
+        self.legal_difficulty_level: List[int] = []
 
     @abc.abstractmethod
-    def legal_difficulty_levels(self, previous_difficulty_level: int) -> List[int]:
+    def legal_difficulty_levels(self,
+                                previous_difficulty_level: int) -> List[int]:
         """
         :return: the accessible index of difficulty level, keep or upgrade difficulty level
         """
         pass
 
     @abc.abstractmethod
-    def decide_difficulty_level(self, current_game_round: int) -> Dict[str, int]:
+    def decide_difficulty_level(self,
+                                current_game_round: int) -> Dict[str, int]:
         """
         :return: the difficulty setting
         """
         pass
 
 
-class StudentInterface(metaclass=abc.ABCMeta):
+class StudentInterface(AgentAbstractBaseClass):
     @abc.abstractmethod
-    def __init__(self, chinese_phonetic: str, target_english: str, difficulty_setting: Dict[str, int]):
-        """
-        student agent should know the chinese, phonetic, and the target length
+    def __init__(self,
+                 player_id: int,
+                 player_name: str,
+                 chinese_phonetic: str,
+                 target_english: str,
+                 difficulty_setting: Dict[str, int]):
+        super().__init__(player_id, player_name)
+        """Initializes student agent.
+
+        Args:
+            self._CONFUSING_LETTER_DIC: Dictionary, mandatory, the confusing letter definition
+            self.chinese_phonetic: str, student can see the chinese and phonetic
+            self.difficulty_setting: get the difficulty setting from tutor 
+            self.target_length: integer, store the length of spelling
+            self.available_letter: legal letters
+            self.confusing_letter, the chosen confusing letter
+            self.masks, tensor, will be used in spelling 
+            self.stu_spelling, string, initialize student spelling
         """
         self._CONFUSING_LETTER_DIC: Dict[str, List[str]] = {}
         self.chinese_phonetic: str = chinese_phonetic
         self.difficulty_setting: Dict[str, int] = difficulty_setting
-        self.target_length = len(target_english.replace(" ", ""))  # get the length of target english
+        self.target_length = len(target_english.replace(" ", ""))
         self.available_letter: List[str] = target_english.split(' ')
         self.confusing_letter: List[str] = []
         self.masks = None
+        self.stu_spelling: str = ''
 
     @abc.abstractmethod
     def letter_space(self) -> List[str]:
         """
-            # 不管迷惑字母也不管可选择的字母，现在需要学生先给一个答案出来
-            合法的动作包括：正确的拼写字母，迷惑字母，以及选择了一个动作以后，要将该字母从库中删除
-            get legal actions from chance player
             :return: list of legal letters [a,c,d,f,e,b,t,y]
         """
         pass
 
     @abc.abstractmethod
-    def student_spelling(self, student_feedback: List[int]) -> List[str]:
+    def student_spelling(self,
+                         student_feedback:  Union[None, Dict[str, int]]) -> List[str]:
         """
         :return: student spelling
         """
         pass
 
 
-class ExaminerInterface(metaclass=abc.ABCMeta):
+class ExaminerInterface(AgentAbstractBaseClass):
     """
     input: students spelling, correct spelling
 
-        :return:  students feedback: {letter, color}
+        :return:  students feedback: {letter, color_index}
                   tutor feedback: [accuracy, completeness, attempts,....]
         """
 
-    def __init__(self):
+    def __init__(self,
+                 player_id: int,
+                 player_name: str):
+        super().__init__(player_id, player_name)
+        """Initializes examiner agent.
+
+        Args:
+            self.student_feedback: Dictionary, mandatory, store students' feedback
+            self.tutor_feedback: list, store tutor feedback
         """
-            initialize the student feedback, tutor feedback
-                """
         self.student_feedback: Dict[str, int] = {}
         self.tutor_feedback: List[float] = []
 
     @abc.abstractmethod
-    def give_feedback(self, student_spelling: str, correct_spelling: str) -> Tuple[Dict[str, int], List[float]]:
+    def give_feedback(self,
+                      student_spelling: str,
+                      correct_spelling: str) -> Tuple[Dict[str, int], List[float]]:
         """
-        :return:  students feedback: {letter, color}
+        :return:  students feedback: {letter, color_index}
                   tutor feedback: [accuracy, completeness, attempts,....]
         """
         pass
