26afa812fbc518b457aa3d1a0c2833d1efdafb74
SryMkr <73199151+SryMkr@users.noreply.github.com>
2023-07-20 08:29:03 +0100

    Update re_preprocess.py

--- Vocabulary_Learning_Framework/utils/re_preprocess.py
+++ Vocabulary_Learning_Framework/utils/re_preprocess.py
@@ -1,34 +1,25 @@
 """
 purposes: (1)provide the interface to build costuming data
-          (2)one example of process data
-          (3) save to json file
+          (2) save data to json file
+要把四种单词信息按顺序保存到文件中，为了避免重复意思，最好保存为字典的形式，这样每一个单词只对应一个意思
 
-就是提供一个文件，可以自己处理自己的数据并且保存到文件中，最后保存的的文件格式一样
 1: 不同单词书有其各自的单词，但是【句子，图片，视频，发音】都可以共用，放在各自的总文件夹下，学习任何一个单词都可以直接调用
 2：当前项目为了简化，只处理【中文，音标，词性 英文拼写】，其他的信息提供调用方法
 
 Simon说会议的主要重点是：（1）建立interface （2）定义好用的类型 （3）history information是包括过去所有的单词信息
-所有的输入和输出都有自己的数据类型，以及都有自己的模板
-
-1: 研究人员肯定不需要自己准备数据的，所以只要输入单词书的名字就可以
-2：研究人员只需要输入想要的信息就可以得到固定的信息格式 【book name, information】
-3：环境里面可以返回单词书以及信息的格式，还有各种有关预处理结果数据的信息  【查看书名，单词信息】
-
-
 
 """
 import re
 import io
 import os
 import abc
-from phonetic_process import get_phonetic_components
-from typing import List
+from phonetic_process import get_phonemes
 import json
 
 
 class PreProcessBookInterface(metaclass=abc.ABCMeta):
     """Abstract base class for customizing vocabulary data.
-    save vocab data to json file
+       save vocab data to json file
     """
 
     @abc.abstractmethod
@@ -39,24 +30,23 @@ class PreProcessBookInterface(metaclass=abc.ABCMeta):
         """Initialize parameters.
 
             Args:
-                self._raw_file_path: str, mandatory. for costuming data. read from file
-                self._write_file_path: str, mandatory. write to file
+                self._raw_file_path: str, mandatory. for costuming data. raw data path
+                self._write_file_path: str, mandatory. write to target file
                 self._vocab_data, vocab data, format [chinese, pos, [phonemes], English]  for example
                 ['丢弃', 'vt', ['d', 'ɪ', 's', 'k', 'ɑ', 'r', 'd'], 'discard']
                 **agent_specific_kwargs: optional extra args.
                 """
         self._raw_file_path = vocab_book_raw_data_path
         self._write_file_path = vocab_book_write_path
-        self._vocab_data: List[List[str, str, List[str], str]] = []
 
     @abc.abstractmethod
     def preprocess_book(self):
         """
         write your own code to preprocess data, the order must be in line with [chinese, pos, [phonemes], English]
+        # 之所以是这种格式是因为，后期还要选择要什么信息
         for example ['丢弃', 'vt', ['d', 'ɪ', 's', 'k', 'ɑ', 'r', 'd'], 'discard']
         save self._vocab_data to json file
         """
-        self._vocab_data = []
 
 
 class PreProcessBook(PreProcessBookInterface):
@@ -72,7 +62,7 @@ class PreProcessBook(PreProcessBookInterface):
         search_pattern = r'[()⋯*《》]'  # search special symbols
         part_of_speech = ['vt', 'a', 'n', 'ad', 'vt', 'vi', 'prep', 'pron', 'conj', 'aux']  # all part of speech
         match_pattern = r'[a-z]+\s+[a-z]+\s+[\u4e00-\u9fff]+'  # match my data pattern
-        results = {}  # store results
+        results = {}  # store results，为了去除多意思单词
         with io.open(self._raw_file_path, encoding="utf8") as file:
             for line in file:
                 line = line.lower().strip()  # lower letter
@@ -83,7 +73,7 @@ class PreProcessBook(PreProcessBookInterface):
                         result = matches.group(0)
                         result = re.sub(r'\s+', ' ', result)
                         result_list = result.split(' ')  # en, pos, ch
-                        _, word_phonetic = get_phonetic_components(result_list[0])
+                        _, word_phonetic = get_phonemes(result_list[0])
                         results[result_list[2]] = [result_list[1], word_phonetic, result_list[0]]
         json_str = json.dumps(results)
         with open(self._write_file_path, 'w') as j_file:
