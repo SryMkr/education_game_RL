51e1479d8984985414434287a87a8a51da4b4815
SryMkr <73199151+SryMkr@users.noreply.github.com>
2023-08-08 07:55:29 +0100

    Add files via upload

--- Vocabulary_Learning_Framework/agents_interface.py
+++ Vocabulary_Learning_Framework/agents_interface.py
@@ -7,11 +7,13 @@
                        B: a policy get the observation and provide the action probabilities, then agent select an action based on probabilities
                        (observation->action probabilities->action)
     compared with the (uniform_random) agent that have different policy
+
+
+思考将agent，可以分为直接初始化，然后结合策略，选择动作的道路
 """
 
 import abc
-from typing import List, Tuple, Dict, Optional
-from torch import Tensor
+from typing import List, Tuple, Dict
 import random
 
 
@@ -26,12 +28,24 @@ class AgentAbstractBaseClass(metaclass=abc.ABCMeta):
         """Initializes agent.
 
                 Args:
-                    player_id: integer, mandatory.
+                    player_id: zero-based integer，for index agent
                     player_name: string.
                     **agent_specific_kwargs: optional extra args.
                 """
         self._player_id: int = player_id
         self._player_name: str = player_name
+
+    @abc.abstractmethod
+    def step(self, time_step):
+        """
+           Agents should handle `time_step` and extract the required part of the
+           `time_step.observations` field.
+
+           Arguments:
+             time_step: an instance of rl_environment.TimeStep.
+           Returns:
+             A `StepOutput` for the current `time_step`. (an action or information) !!!!!!!!!!!
+           """
 
     @property
     def player_id(self) -> int:
@@ -50,60 +64,26 @@ class AgentAbstractBaseClass(metaclass=abc.ABCMeta):
 
 class SessionCollectorInterface(AgentAbstractBaseClass):
     """
-    use 'random' or 'sequential' slice vocabulary data into sessions
-    :return: self._vocabulary_session: sliced data by specific method
+    是为了组合学习新单词和组合旧单词的功能
+    怎么能够增加一些随机性呢？让每个agent都有一定的策略可以使用，确定展示那个session呗,如何构造legal actions?
     """
 
     @abc.abstractmethod
     def __init__(self,
                  player_id: int,
-                 player_name: str,
-                 vocabulary_data: List[List[str]],
-                 new_words_number: int,
-                 # review_words_number: int,
-                 # review_selection_method: str
-                 new_selection_method: str):
+                 player_name: str
+                 ):
         super().__init__(player_id, player_name)
         """Initializes TaskCollector agent.
-
-        Args:
-            self._vocabulary_data: the whole vocabulary data
-            self._new_words_number: the new words number in each session
-            self._new_selection_method: the selection method regarding new words
-            self._review_words_number: the review words number in each session
-            self._review_selection_method: the selection method of review words
-            self._vocabulary_session: sliced data by specific method
-            self._current_session: after finishing each session, go next session
+        :arg
+            self._action: the selected action
         """
-        self._vocabulary_data: List[List[str]] = vocabulary_data
-        self._new_words_number: int = new_words_number
-        # self._review_words_number: int = review_words_number
-        self._new_selection_method: str = new_selection_method
-        # self._review_selection_method: str = review_selection_method
-        self._vocabulary_session: List[List[List[str]]] = []
-
-    def sequential_method(self):
-        """the sequential method of new words"""
-        for i in range(0, len(self._vocabulary_data), self._new_words_number):
-            vocabulary_pieces = self._vocabulary_data[i:i + self._new_words_number]
-            self._vocabulary_session.append(vocabulary_pieces)
+        self._action: int = 0
 
     @abc.abstractmethod
-    def random_method(self):
-        """the random method of new words"""
-
-    @abc.abstractmethod
-    def piece_data(self):
+    def step(self, time_step) -> List[List[str]]:
         """
-            当前先不考虑复习单词的事，就是单词表的挑选方式
-            :return: the sessions with different methods
-            """
-        pass
-
-    @abc.abstractmethod
-    def session_collector(self, current_session) -> List[List[str]]:
-        """
-                    :return: select one session words data from vocabulary books over time
+                    :return: the number of selected session
                     """
         pass
 
@@ -115,45 +95,44 @@ class PresentWordInterface(AgentAbstractBaseClass):
     def __init__(self,
                  player_id: int,
                  player_name: str,
-                 session_data: List[List[str]],
                  selection_method: str,
                  ):
         super().__init__(player_id, player_name)
         '''
         :args:
-                self._session_data: get from sessioncollector agent
+               
                 self._task: a specific task for student agent
                 self._selection_method: the method of selecting task 
                 self._difficulty_session_data: sort from easy to hard
                 self._shuffle_list: copy original data to shuffle
         '''
-        self._session_data = session_data
+
         self._task: List[str] = []
         self._selection_method = selection_method
         self._difficulty_session_data = sorted(self._session_data, key=lambda x: len(x[-1]))
         self._shuffle_list = self._session_data[:]
         random.shuffle(self._shuffle_list)
 
-    def sequential_method(self):
+    def sequential_method(self, session_data):
         """ sequence method
           对于顺序选择来说，将第一个选择出来并移到末尾，如果正确了将其移除就好，反正永远只选第一个"""
-        self._task = self._session_data[0]
-        self._session_data = self._session_data[1:] + [self._session_data[0]]
+        self._task = session_data[0]
+        self._session_data = session_data[1:] + [session_data[0]]
 
-    def random_method(self):
+    def random_method(self, session_data):
         """ random method"""
         self._task = self._shuffle_list[0]
         self._shuffle_list = self._shuffle_list[1:] + [self._shuffle_list[0]]
         self._session_data = self._shuffle_list
 
-    def easy_to_hard(self):
+    def easy_to_hard(self, session_data):
         """ the definition of difficulty of task is the length of task
            对于从简单到难来说，先按照字母长度排序，然后就是按照顺序选择，直到答对"""
         self._task = self._difficulty_session_data[0]
         self._difficulty_session_data = self._difficulty_session_data[1:] + [self._difficulty_session_data[0]]
         self._session_data = self._difficulty_session_data
 
-    def DDA(self):
+    def DDA(self, session_data):
         """ the dynamic difficulty adjustment method, do not achieve currently"""
         pass
 
@@ -161,22 +140,24 @@ class PresentWordInterface(AgentAbstractBaseClass):
     def session_data(self) -> List[List[str]]:
         return self._session_data
 
-    def select_task(self) -> List[str]:
+    def step(self, time_step) -> (List[List[str]], List[str]):
         """
         if correctly answer at hardest test level, the agent need to remove the task from session
+        :rtype: object
         :returns the task"""
 
         if self._selection_method == 'sequential':
-            self.sequential_method()
+            self.sequential_method(time_step.observations["vocab_session"])
         elif self._selection_method == 'random':
-            self.random_method()
+            self.random_method(time_step.observations["vocab_session"])
         elif self._selection_method == 'easy_to_hard':
-            self.easy_to_hard()
+            self.easy_to_hard(time_step.observations["vocab_session"])
         elif self._selection_method == 'DDA':
-            self.DDA()
-        return self._task
+            self.DDA(time_step.observations["vocab_session"])
+        return self._session_data, self._task
 
 
+'''
 class TutorInterface(AgentAbstractBaseClass):
     @abc.abstractmethod
     def __init__(self,
@@ -215,61 +196,74 @@ class TutorInterface(AgentAbstractBaseClass):
         :return: the difficulty setting
         """
         pass
+'''
 
 
 class StudentInterface(AgentAbstractBaseClass):
+    """ student agent can see conditions(chinese,phonetic,POS) -> provide spelling,
+    optional information: answer length, available letter,
+    ['蜘蛛 n s p aɪ d ɝ', 's p i d e r']
+    至少可以先实现一个随机的拼写，然后把与环境的交互定义好，完成一次整体的流程
+    """
+
     @abc.abstractmethod
     def __init__(self,
                  player_id: int,
                  player_name: str,
-                 chinese_phonetic: str,  # ！！！！！！！直接掩盖掉，还是加一些噪声？
-                 target_english: str,
-                 difficulty_setting: Dict[str, int]):
+                 stu_type: str,
+                 word_length: int,
+                 spelling_condition: str,
+                 stu_feedback: List[str]):
         super().__init__(player_id, player_name)
         """Initializes student agent.
 
         Args:
-            self._CONFUSING_LETTER_DIC: Dictionary, mandatory, the confusing letter definition
-            self.chinese_phonetic: str, student can see the chinese and phonetic
-            self.difficulty_setting: get the difficulty setting from tutor 
-            self.target_length: integer, store the length of spelling
-            self.available_letter: legal letters
-            self.confusing_letter, the chosen confusing letter
-            self.masks, tensor, will be used in spelling, learn from feedback 
-            self.stu_spelling, string, initialize student spelling
-            self.stu_feedback, Dict, getting from examiner
+            self._spelling_condition: str, mandatory, what condition (chinese,phonetic,POS) does student agent can see
+            self._stu_spelling: student provide spelling based on condition
+            self._stu_learn: student train n-grams based on feedback
+            self._letter_space: available letter
+            self._stu_type: student type: random, perfect, forget
+            self._word_length: int, word length for control random student
         """
-        self._CONFUSING_LETTER_DIC: Dict[str, List[str]] = {}
-        self.chinese_phonetic: str = chinese_phonetic
-        self.difficulty_setting: Dict[str, int] = difficulty_setting
-        self.target_length: int = len(target_english.replace(" ", ""))
-        self.target_spelling: str = target_english
-        self.available_letter: List[str] = target_english.split(' ')
-        self.confusing_letter: List[str] = []
-        self.masks: Optional[Tensor] = None
-        self.stu_spelling: str = ''
-        self.stu_feedback: Optional[Dict[str, int]] = {}
+        self._spelling_condition: str = spelling_condition
+        self._stu_spelling: List[str] = []
+        self._stu_feedback: List[str] = stu_feedback
+        self._letter_space: List[str] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+                                         'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        self._stu_type = stu_type
+        self._word_length = word_length
 
-    @abc.abstractmethod
-    def letter_space(self) -> List[str]:
-        """
-         only correct letter ot a group of confusing letter and correct letter
-            :return: list of legal letters [a,c,d,f,e,b,t,y]
-        """
+    def random_stu(self):
+        """student does learn and forget"""
+        for letter_position in range(self._word_length):
+            letter = random.choice(self._letter_space)
+            self._stu_spelling.append(letter)
+
+    def perfect_stu(self):
+        """student learn but does not forget"""
+        pass
+
+    def forget_stu(self):
+        """student learn but forget: (1) be a perfect student (2) add noise to simulate forget overtime"""
         pass
 
     @abc.abstractmethod
-    def student_spelling(self,
-                         student_feedback: Optional[Dict[str, int]]) -> List[str]:
+    def stu_spelling(self) -> List[str]:
         """
         :return: student spelling
         """
-        pass
+        if self._stu_type == 'random':
+            self.random_stu()
+        elif self._stu_type == 'perfect':
+            self.perfect_stu()
+        elif self._stu_type == 'forget':
+            self.forget_stu()
+        return self._stu_spelling
 
     @abc.abstractmethod
-    def student_memorizing(self) -> None:
+    def stu_learn(self) -> None:
         """
-        :return: for students memorizing once difficulty level changes
+        update n-grams based on feedback
         """
         pass
 
@@ -283,8 +277,8 @@ class ExaminerInterface(AgentAbstractBaseClass):
         super().__init__(player_id, player_name)
         """Initializes examiner agent.
         Args:
-            self.student_feedback: Dictionary, mandatory, store students' feedback
-            self.tutor_feedback: list, store tutor feedback
+            self.examiner_feedback: [accuracy, completeness, letters]
+            
         """
         self.examiner_feedback: List[int, int, List[int]] = []
 
@@ -293,6 +287,6 @@ class ExaminerInterface(AgentAbstractBaseClass):
                       student_spelling: str,
                       correct_spelling: str) -> Tuple[Dict[str, int], List[float]]:
         """
-          :return:  Examiner feedback: [accuracy, completeness, letter_judgement]
+          :return:  Examiner feedback: [accuracy, completeness, letters]
         """
         pass
